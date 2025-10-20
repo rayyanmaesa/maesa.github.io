1e9277fa712051707d2125aa9fbbfd877693d150
rayyanmaesa <rayyanmaesa@gmail.com>
2025-10-20 21:10:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     bars.forEach(bar=>{
       const y = canvas.height - (Math.sin(bar.phase)*bar.height + bar.height);
-      ctx.fillStyle = `rgba(204,0,255,0.6)`; // neon ungu
+      ctx.fillStyle = `#d46509`; // neon ungu
       ctx.fillRect(bar.x, y, canvas.width/barCount*0.6, bar.height);
       bar.phase += bar.speed;
     });
